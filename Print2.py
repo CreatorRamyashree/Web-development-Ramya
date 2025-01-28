6c4039fcf0f61b9116e4e5fb133c29ac54bc6f65
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-01-28 17:34:40 +0100

    Add files via upload

---
+++ Print2.py
@@ -0,0 +1,36 @@
+print("Welcome to Python learning!")
+
+print(50000)
+
+print("Swathi mam\nRamya")
+
+print("Teja",10)
+
+print("Welcome to", end= "*")
+
+
+# creating different variables
+
+x = 8
+y = "Lucky"
+
+print(x)
+print(y)
+
+Codingal = "Coding"
+print(Codingal)
+
+# user input
+name = input("Enter your full name: ")
+
+print("\nHello", name,"welcome to the coding world")
+
+
+
+
+import keyword
+
+# print all the python keywords
+
+print("Python keywords are...\n")
+print(keyword.kwlist)
