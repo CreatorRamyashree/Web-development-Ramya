af7a80584f949359213d57e2e20a7a78b2f1e1f4
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-01-31 18:29:10 +0100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,46 @@
+str1 = 'Ramyashree'
+str2 = 'Tejashree'
+
+print('str1 + str2 = ', str1 + str2)
+
+str = "Swathi mam"
+print(str[::3])
+print(str[0::3])
+print(str[::-1])
+
+
+
+
+# activity 3
+
+text= str(input("Enter a string:  "))
+
+# reverse string
+revText = text[::-1]
+text = revText
+
+print("Reverse of the Given String is:")
+print(text)
+
+# activity 1, 2
+a = 5
+print("type of a:", type(a))
+z = float(a)
+print("type of z:", type(z))
+
+b = 2.5
+print("type of b:", type(b))
+f = str(b)
+print("type of f:", type(f))
+
+c = "coding"
+print("type of c:", type(c))
+g = int(c)
+print("type of g:", type(g))
+
+d = True
+print("type of d:", type(d))
+s = bool(d)
+print("type of s:", type(s))
+
+
