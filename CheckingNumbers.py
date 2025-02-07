0bab9f6865668aab8c48def1c2eb44bc373cc2bd
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-02-07 18:28:41 +0100

    Add files via upload

---
+++ CheckingNumbers.py
@@ -0,0 +1,13 @@
+
+i = int(input("enter a number: "))
+if (i < 15):
+    print("i is smaller that 15")
+    print("i'm in the if block")
+
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
+
+print("i'm not in if and in the else block")
+
+
