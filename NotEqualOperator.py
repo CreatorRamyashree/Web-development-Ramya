de8cd7b38df0980b5848a05ff3730a50a52a4827
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-02-11 17:44:15 +0100

    Add files via upload

---
+++ NotEqualOperator.py
@@ -0,0 +1,25 @@
+a = 25
+b = 10
+c = 10
+
+print(a != b)
+print(b != c)
+
+a = "swimming"
+b = "pool"
+
+if a != b:
+    print(a, 'and', b, 'are different.')
+
+a = 4
+b = 14
+
+if (a == 1) != (b == 14):
+    print('Namaste')
+
+a = int(input("enter a number: "))
+
+if a%2 != 0:
+    print(a, "it is an odd number.")
+else:
+    print("It is an even number.")
