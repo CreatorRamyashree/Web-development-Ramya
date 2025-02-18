4fcbe2432cfea95b53d8d47c161429c9fa60c21d
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-02-18 17:23:05 +0100

    Add files via upload

---
+++ ReverseAstring.py
@@ -0,0 +1,11 @@
+string = input("Enter your own String: ")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Orginal String =", string)
+print("The Reversed String = ", string2)
+
+
+
